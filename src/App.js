f7f480b33f2a00bb7d44c383cb4c350c1dc6540e
Brad <bkhoffman@gmail.com>
2019-07-09 00:20:20 -0500

    update to style

--- src/App.js
+++ src/App.js
@@ -7,27 +7,28 @@ import HeroCard from "./Components/HeroCard";
 import heroes from "./heroes.json";
 
 class App extends Component {
-
+  //set state
   state = {
     heroes,
     score: 0,
     highscore: 0,
     message: ""
   }
-
+  //checks score to see in new highscore, if true set new state
   scoreReset = () => {
     if (this.state.score > this.state.highscore) {
       this.setState({ highscore: this.state.score }, function () {
         console.log(this.state.highscore);
       });
     }
+    //change each hero clicked property back to false, set score back to 0
     this.state.heroes.forEach(hero => {
       hero.clicked = false;
     });
     this.setState({ score: 0 });
     return true;
   }
-
+  //on click, use filter method grab id, if "not clicked/false", set to true and add to score
   clickCheck = id => {
     const heroClicked = this.state.heroes.filter(card => card.id === id);
     if (!heroClicked[0].clicked) {
@@ -37,6 +38,7 @@ class App extends Component {
         message: "Correct!"}, function () {
         console.log(this.state.score);
       });
+      //use sort method and math random to change the layout order
       this.state.heroes.sort(() => Math.random() - 0.5)
       return true;
     } else {
@@ -50,9 +52,9 @@ class App extends Component {
   // Map over this.state.heroes and render a herohero component for each hero object
   render() {
     return (
-      //maybe move header out of wrapper
+      <div>
+      <Header  message={this.state.message} score={this.state.score} highscore={this.state.highscore}>Marvel Clicky Game</Header>
       <Wrapper> 
-        <Header  message={this.state.message} score={this.state.score} highscore={this.state.highscore}>Marvel Clicky Game</Header>
         {this.state.heroes.map(hero => (
           <HeroCard
           id={hero.id}
@@ -62,6 +64,7 @@ class App extends Component {
           />
         ))}
       </Wrapper>
+      </div>
     );
   }
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Header(props){
   return (
     <div className="header">
-      <span className="title">Superhero Clicky Game</span>
+      <span className="title">Marvel Clicky Game</span>
       <span className="message">{props.message}</span>
       <span className="scores">Score: {props.score} | Highscore: {props.highscore}
       </span>
